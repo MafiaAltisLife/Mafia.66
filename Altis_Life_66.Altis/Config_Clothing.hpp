e9bf3790389ea60439ea1d83e8110ae61d837036
Tranks <thalles_gteduardo@icloud.com>
2016-02-16 01:47:22 -0200

    Update Config_Clothing.hpp

--- Altis_Life_66.Altis/Config_Clothing.hpp
+++ Altis_Life_66.Altis/Config_Clothing.hpp
@@ -299,6 +299,7 @@ class Clothing {
         vests[] = {
             { "NONE", "Remove Vest", 0, { "", "", -1 } },
             { "V_Rangemaster_belt", "Rangemaster Belt", 2000, { "", "", -1 } }
+            { "V_BandollierB_blk", "Slash Bandolier (Black)", 2000, { "", "", -1 } },
         };
         backpacks[] = {
             { "NONE", "Remove Backpack", 0, { "", "", -1 } },
