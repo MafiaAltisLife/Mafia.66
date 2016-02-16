3aae34966084c5367fb0a7c090102800a0f3da10
Tranks <thalles_gteduardo@icloud.com>
2016-02-16 00:59:02 -0200

    Update Config_Clothing.hpp

--- Altis_Life_66.Altis/Config_Clothing.hpp
+++ Altis_Life_66.Altis/Config_Clothing.hpp
@@ -76,7 +76,7 @@ class Clothing {
         vests[] = {
             { "NONE", "Remove Vest", 0, { "", "", -1 } },
             { "V_AssaultPack_khk", "", 2000, { "", "", -1 } },
-            { "V_Press_F", "", 3000, { "", "", -1 } },
+            { "V_Press_F", "", 3000, { "", "", -1 } }
             
         };
         backpacks[] = {
