a2fea039fe197a250be0315c02cebd7a676aea85
Tranks <thalles_gteduardo@icloud.com>
2016-02-18 00:56:53 -0200

    Update Config_Clothing.hpp

--- Altis_Life_66.Altis/Config_Clothing.hpp
+++ Altis_Life_66.Altis/Config_Clothing.hpp
@@ -115,7 +115,8 @@ class Clothing {
         side = "cop";
         uniforms[] = {
             { "NONE", "Remove Uniform", 0, { "", "", -1 } },
-            { "U_B_CombatUniform_mcam", "", 25, { "", "textures\Cop\policia_roupa.paa", -1 } }
+            { "U_B_CombatUniform_mcam", "", 25, { "", "textures\Cop\policia_roupa.paa", -1 } },
+            { "U_B_CombatUniform_mcam", "", 25, { "", "textures\Cop\elite.paa", 3 } }
         };
         headgear[] = {
             { "NONE", "Remove Hat", 0, { "", "", -1 } },
