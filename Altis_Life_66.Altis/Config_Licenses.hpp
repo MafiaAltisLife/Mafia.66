78f857c7f6fc7faf44ed1452ba90e5171de09b85
Tranks <thalles_gteduardo@icloud.com>
2016-02-25 03:32:17 -0300

    Update Config_Licenses.hpp

--- Altis_Life_66.Altis/Config_Licenses.hpp
+++ Altis_Life_66.Altis/Config_Licenses.hpp
@@ -157,7 +157,7 @@ class Licenses {
     class rebel {
         variable = "rebel";
         displayName = "STR_License_Rebel";
-        price = 500000;
+        price = 1000000;
         illegal = true;
         side = "civ";
     };
