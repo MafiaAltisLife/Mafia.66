ac607fc776dd6c4448c8a9f5e976aa4ace6208e5
Tranks <thalles_gteduardo@icloud.com>
2016-02-25 02:32:32 -0300

    Update Config_vItems.hpp

--- Altis_Life_66.Altis/Config_vItems.hpp
+++ Altis_Life_66.Altis/Config_vItems.hpp
@@ -23,6 +23,14 @@ class VirtualShops {
         level[] = { "", "", -1, "" };
         items[] = { "waterBottle", "rabbit", "apple", "redgull", "tbacon", "lockpick", "pickaxe", "fuelFull", "peach", "boltcutter", "blastingcharge" };
     };
+    
+    class doador {
+        name = "Mercado";
+        side = "civ";
+        license = "donator";
+        level[] = { "", "", -1, "" };
+        items[] = { "waterBottle", "rabbit", "apple", "redgull", "tbacon", "lockpick", "pickaxe", "fuelFull", "peach", "boltcutter", "blastingcharge" };
+    };
 
     class gang {
         name = "STR_Shops_Gang";
