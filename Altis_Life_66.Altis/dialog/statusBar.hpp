0bd27d5050b8918826536c7338d786ce6da8f3e7
Jamal Ismail <gamau@msn.com>
2016-02-27 02:27:26 -0300

    Update statusBar.hpp

--- Altis_Life_66.Altis/dialog/statusBar.hpp
+++ Altis_Life_66.Altis/dialog/statusBar.hpp
@@ -1,33 +1,34 @@
 #define ST_RIGHT 0x01
+
 class osefStatusBar {
- idd = -1;
- onLoad = "uiNamespace setVariable ['osefStatusBar', _this select 0]";
- onUnload = "uiNamespace setVariable ['osefStatusBar', objNull]";
- onDestroy = "uiNamespace setVariable ['osefStatusBar', objNull]";
- fadein = 0;
- fadeout = 0;
- duration = 10e10;
- movingEnable = 0;
- controlsBackground[] = {};
- objects[] = {};
- class controls {
- class statusBarText {
- idc = 555556;
- x = safezoneX + safezoneW - 0.85;
- y = safezoneY + safezoneH - 0.08;
- w = 0.85;
- h = 0.056;
- shadow = 2;
- colorBackground[] = { 0, 0, 0, 0.5 };
- font = "PuristaSemibold";
- size = 0.03;
- type = 13;
- style = 1;
- text="";
- class Attributes {
- align="left";
- color = "#CCCCCC";//#5fe60c
- };
- };
- };
+	idd = -1;
+	onLoad = "uiNamespace setVariable ['osefStatusBar', _this select 0]";
+	onUnload = "uiNamespace setVariable ['osefStatusBar', objNull]";
+	onDestroy = "uiNamespace setVariable ['osefStatusBar', objNull]";
+	fadein = 0;
+	fadeout = 0;
+	duration = 10e10;
+	movingEnable = 0;
+	controlsBackground[] = {};
+	objects[] = {};
+	class controls {
+		class statusBarText {
+			idc = 1000;
+			x = safezoneX + safezoneW - 1;
+			y = safezoneY + safezoneH - 0.08;
+			w = 1;
+			h = 0.04;
+			shadow = 2;
+			colorBackground[] = { 1, 0.3, 0, 0.0 };  // uncomment and increase 4th number to have a background
+			font = "PuristaSemibold";
+			size = 0.03;
+			type = 13;
+			style = 1;
+			text="Carregando informações do servidor...";
+			class Attributes {
+				align="right";
+				color = "#fff";
+			};
+		};
+	};
 };
