afe9256711d539904bbed547fad7dd28b66b7ee3
Tranks <thalles_gteduardo@icloud.com>
2016-02-26 23:48:56 -0300

    Update Config_Vehicles.hpp

--- Altis_Life_66.Altis/Config_Vehicles.hpp
+++ Altis_Life_66.Altis/Config_Vehicles.hpp
@@ -117,8 +117,8 @@ class CarShops {
             { "C_Offroad_01_F", 5000, { "" }, { "", "OffroadPM", -1 } },
             { "C_SUV_01_F", 20000, { "" }, { "", "SuvPM", -1 } },
             { "C_Hatchback_01_sport_F", 30000, { "" }, { "life_coplevel", "HatchPM", -1 } },
-            { "B_MRAP_01_F", 200000, { "" }, { "life_coplevel", "SCALAR", -1 } }
-            { "B_G_Offroad_01_armed_F", 1000000, { "" }, { "", "", -1 } },
+            { "B_MRAP_01_F", 200000, { "" }, { "life_coplevel", "SCALAR", -1 } },
+            { "B_G_Offroad_01_armed_F", 1000000, { "" }, { "", "", -1 } }
         };
     };
 
