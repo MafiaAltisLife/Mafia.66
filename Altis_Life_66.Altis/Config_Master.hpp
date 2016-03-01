41c79cfc44493f8f7fb0fb705690f77f343bc0d7
digone85 <rodigone@gmail.com>
2016-03-01 19:06:07 -0300

    Update Config_Master.hpp

--- Altis_Life_66.Altis/Config_Master.hpp
+++ Altis_Life_66.Altis/Config_Master.hpp
@@ -17,14 +17,14 @@ class Life_Settings {
     clothing_masks[] = { "H_Shemag_olive", "H_Shemag_khk", "H_Shemag_tan", "H_Shemag_olive_hs", "H_ShemagOpen_khk", "H_ShemagOpen_tan", "G_Balaclava_blk", "G_Balaclava_combat", "G_Balaclava_lowprofile", "G_Balaclava_oli", "G_Bandanna_aviator", "G_Bandanna_beast", "G_Bandanna_blk", "G_Bandanna_khk", "G_Bandanna_oli", "G_Bandanna_shades", "G_Bandanna_sport", "G_Bandanna_tan", "U_O_GhillieSuit", "U_I_GhillieSuit", "U_B_GhillieSuit", "H_RacingHelmet_1_black_F", "H_RacingHelmet_1_red_F", "H_RacingHelmet_1_white_F", "H_RacingHelmet_1_blue_F", "H_RacingHelmet_1_yellow_F", "H_RacingHelmet_1_green_F", "H_RacingHelmet_1_F", "H_RacingHelmet_2_F", "H_RacingHelmet_3_F", "H_RacingHelmet_4_F" };
 
     /* Cop related settings */
-    cops_online_min = 5; //minimum cops online for robbing a bank
+    cops_online_min = 7; //minimum cops online for robbing a bank
 
     /* Medic related settings*/
     allow_medic_weapons = true; // true allows medics to hold/use weapons - false disallows
 
     /* Revive system settings */
     revive_cops = false; //true to enable cops the ability to revive everyone or false for only medics/ems.
-    revive_fee = 5000; //Revive fee that players have to pay and medics / EMS are rewarded
+    revive_fee = 20000; //Revive fee that players have to pay and medics / EMS are rewarded
 
     /* House related settings */
     house_limit = 5; //Maximum amount of houses a player can own.
@@ -50,9 +50,9 @@ class Life_Settings {
     bank_med = 150000; //Amount of cash on bank for new medics
 
     /* Paycheck Amount */
-    paycheck_cop = 5000; //Payment for cops
+    paycheck_cop = 25000; //Payment for cops
     paycheck_civ = 5000; //Payment for civillians
-    paycheck_med = 5000; //Payment for medics
+    paycheck_med = 10000; //Payment for medics
 
     /* Federal Reserve settings */
     noatm_timer = 10; //Time in minutes that players won't be able to deposit his money after selling stolen gold
